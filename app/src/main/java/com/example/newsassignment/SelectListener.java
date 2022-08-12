f8437e65f2579e531918b8ab627873d2b6a11f4d
B180U <timothy.bahari@gmail.com>
2022-08-12 19:51:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/newsassignment/SelectListener.java
@@ -0,0 +1,7 @@
+package com.example.newsassignment;
+
+import com.example.newsassignment.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
